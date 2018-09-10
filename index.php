c3f1342fc7364d16571d467899be05b8ac2761bd
codetr <manusia.tados@gmail.com>
2018-09-10 16:50:37 +0800

    update

--- index.php
+++ index.php
@@ -128,7 +128,7 @@
                         
                         $options[] = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("MULAI", 'mulai');
                         $question['image'] = "https://scontent-atl3-1.cdninstagram.com/vp/d028c1f665944cf64f24d03edd8818b6/5C18755A/t51.2885-15/e35/37629924_825187871202623_3854795657114025984_n.jpg";
-                        $question['text'] = "Hi ".$profile['displayName'].", Selamat datang di informasi matakuliah mahasiswa STMIK Bumigora Mataram";
+                        $question['text'] = "Hi, Selamat datang di informasi matakuliah mahasiswa STMIK Bumigora Mataram";
                         $buttonTemplate = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("JADWAL KULIAH", $question['text'], $question['image'], $options);
                         
                         // build message
