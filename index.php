dc164321a5037f0e4f6b54c844f5599bde528afc
codetr <manusia.tados@gmail.com>
2018-09-11 23:20:03 +0800

    update

--- index.php
+++ index.php
@@ -78,10 +78,10 @@
                             
                             return $result->getHTTPStatus() . ' ' . $result->getRawBody();
                         }else{
-                            $options[] = new MessageTemplateActionBuilder("JADWAL KULIAH", 'mulai');
+                            $options[] = new MessageTemplateActionBuilder("MULAI", 'mulai');
                             $question['image'] = "https://scontent-atl3-1.cdninstagram.com/vp/d028c1f665944cf64f24d03edd8818b6/5C18755A/t51.2885-15/e35/37629924_825187871202623_3854795657114025984_n.jpg";
-                            $question['text'] = "Klik 'Mulai'";
-                            $buttonTemplate = new ButtonTemplateBuilder("MULAI", $question['text'], $question['image'], $options);
+                            $question['text'] = "Klik 'Mulai' untuk melanjutkan";
+                            $buttonTemplate = new ButtonTemplateBuilder("JADWAL KULIAH", $question['text'], $question['image'], $options);
                             
                             // build message
                             $messageBuilder = new TemplateMessageBuilder("Ada pesan untukmu, pastikan membukanya dengan app mobile Line ya!", $buttonTemplate);
