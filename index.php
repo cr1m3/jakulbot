1e49374007c11950a3803d4b39ef31686c239e0f
codetr <manusia.tados@gmail.com>
2018-09-11 22:43:53 +0800

    update

--- index.php
+++ index.php
@@ -131,10 +131,10 @@
                     {
                         $profile = $res->getJSONDecodedBody();
                         
-                        $options[] = new MessageTemplateActionBuilder("JADWAL KULIAH", 'mulai');
+                        $options[] = new MessageTemplateActionBuilder("MULAI", 'mulai');
                         $question['image'] = "https://scontent-atl3-1.cdninstagram.com/vp/d028c1f665944cf64f24d03edd8818b6/5C18755A/t51.2885-15/e35/37629924_825187871202623_3854795657114025984_n.jpg";
-                        $question['text'] = "Hi ";
-                        $buttonTemplate = new ButtonTemplateBuilder("MULAI", $question['text'], $question['image'], $options);
+                        $question['text'] = "Hi ".$profile['displayName'].", Selamat datang di informasi matakuliah mahasiswa STMIK Bumigora Mataram";
+                        $buttonTemplate = new ButtonTemplateBuilder("JADWAL KULIAH", $question['text'], $question['image'], $options);
                         
                         // $packageId = 2;
                         // $stickerId = 22;
