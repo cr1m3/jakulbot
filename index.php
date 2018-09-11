e9c6d5bf53a940b303fdee8643437721d7fb72df
codetr <manusia.tados@gmail.com>
2018-09-11 22:52:56 +0800

    update

--- index.php
+++ index.php
@@ -133,7 +133,7 @@
                         
                         $options[] = new MessageTemplateActionBuilder("MULAI", 'mulai');
                         $question['image'] = "https://scontent-atl3-1.cdninstagram.com/vp/d028c1f665944cf64f24d03edd8818b6/5C18755A/t51.2885-15/e35/37629924_825187871202623_3854795657114025984_n.jpg";
-                        $question['text'] = "Hi ".$profile['displayName'].", ";
+                        $question['text'] = "Hi ".$profile['displayName'].", Selamat datang di informasi \n Jadwal Kuliah STMIK \n Bumigora Mataram";
                         $buttonTemplate = new ButtonTemplateBuilder("JAKULBOT", $question['text'], $question['image'], $options);
                         
                         // $packageId = 2;
