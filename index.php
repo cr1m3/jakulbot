f9960faa33dc36106f47ee736b4c25e20575eef2
codetr <manusia.tados@gmail.com>
2018-09-13 13:07:59 +0800

    update

--- index.php
+++ index.php
@@ -88,17 +88,9 @@
 
 
 
-                            return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-                        }else{
-                            $options[] = new MessageTemplateActionBuilder("MULAI", 'mulai');
-                            $question['image'] = "https://scontent-atl3-1.cdninstagram.com/vp/d028c1f665944cf64f24d03edd8818b6/5C18755A/t51.2885-15/e35/37629924_825187871202623_3854795657114025984_n.jpg";
-                            $question['text'] = "Hi ".$profile['displayName'].", Selamat datang di informasi Jadwal Kuliah STMIK";
-                            $buttonTemplate = new ButtonTemplateBuilder("JAKULBOT", $question['text'], $question['image'], $options);
-                            $messageBuilder = new TemplateMessageBuilder("Ada pesan untukmu, pastikan membukanya dengan app mobile Line ya!", $buttonTemplate);
-                            $result = $bot->pushMessage($event['source']['userId'], $messageBuilder);
-                            return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+                           
                         }
-                       
+                        return $result->getHTTPStatus() . ' ' . $result->getRawBody();
                     }
                 }
 
